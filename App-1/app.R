756ea52753509b70199504c5d0200afd09d883d2
Dani Blumstein <49007024+DaniBlumstein@users.noreply.github.com>
2024-09-03 11:35:10 -0600

    graphing app editing

--- App-1/app.R
+++ App-1/app.R
@@ -2,12 +2,14 @@ library(rsconnect)
 rsconnect::setAccountInfo(name='104wj0-dani0blumstein', 
                           token='6558579B24BDD9974D8411F722FEFD66', 
                           secret='hf5NT9bDE6WJIFmAY0ure3gTKfwTPkK5MYxnEm9H')
-library(colorspace)
 library(ggplot2)
 library(shiny)
 library(dplyr)
 library(lubridate)
 library(tidyverse)
+
+require(devtools)
+install_version("colorspace", version = "1.4.1", repos = "http://cran.us.r-project.org")
 
 df <- read.csv("data_final.csv")
 
@@ -88,21 +90,26 @@ ui <- fluidPage(
       h4("Additional information"),
       h5("MacManes Lab publications"),
      
-       tags$a(href="https://www.researchgate.net/publication/380424236_SURVIVAL_STRATEGIES_IN_ARID_ENVIRONMENTS_EXPLORING_DESERT_ADAPTATIONS_IN_PEROMYSCUS_EREMICUS?_sg%5B0%5D=cObGFnbBRFqjDYtkZQ6zktVYMF4FyQYMPO-ps878Mr2zhewZ1RUbXdSvITkDSull32FGSe9uhbh8TO7Z63KG7o0UHTXxibxV0ux7K001.sDa4jZ5_DV8xNByyFvE5UF7iyZs5g-iWoU-hPJ6z5QFG0lJy5PE5ZuFJsS9D0lHH8biVW1Hf50BgHy2gyAn0Iw&_tp=eyJjb250ZXh0Ijp7ImZpcnN0UGFnZSI6ImhvbWUiLCJwYWdlIjoicHJvZmlsZSIsInByZXZpb3VzUGFnZSI6InByb2ZpbGUiLCJwb3NpdGlvbiI6InBhZ2VDb250ZW50In19", "Survival Strategies in Arid Environments: Exploring Desert Adaptations in Peromyscus eremicus. PhD dissertation. University of New Hampshire.", target="_blank"),
+      
+      tags$a(href="https://bmcgenomics.biomedcentral.com/articles/10.1186/s12864-024-10629-z", "The multi-tissue gene expression and physiological responses of water deprived Peromyscus eremicus", target="_blank"),
+      br(),
+      tags$a(href="https://www.researchgate.net/publication/380424236_SURVIVAL_STRATEGIES_IN_ARID_ENVIRONMENTS_EXPLORING_DESERT_ADAPTATIONS_IN_PEROMYSCUS_EREMICUS?_sg%5B0%5D=cObGFnbBRFqjDYtkZQ6zktVYMF4FyQYMPO-ps878Mr2zhewZ1RUbXdSvITkDSull32FGSe9uhbh8TO7Z63KG7o0UHTXxibxV0ux7K001.sDa4jZ5_DV8xNByyFvE5UF7iyZs5g-iWoU-hPJ6z5QFG0lJy5PE5ZuFJsS9D0lHH8biVW1Hf50BgHy2gyAn0Iw&_tp=eyJjb250ZXh0Ijp7ImZpcnN0UGFnZSI6ImhvbWUiLCJwYWdlIjoicHJvZmlsZSIsInByZXZpb3VzUGFnZSI6InByb2ZpbGUiLCJwb3NpdGlvbiI6InBhZ2VDb250ZW50In19", "Survival Strategies in Arid Environments: Exploring Desert Adaptations in Peromyscus eremicus. PhD dissertation. University of New Hampshire.", target="_blank"),
+      br(),
+      tags$a(href="https://www.unh.edu/unhtoday/2024/05/desert-mice-offer-insight-potential-climate-change-adaptations", "UNH Today: Desert Mice Offer Insight into Potential Climate Change Adaptations", target="_blank"),
       br(),
       tags$a(href="https://journals-biologists-com.unh.idm.oclc.org/jeb/article/226/23/jeb246924/335759/Parched-cactus-mice-save-water-by-curbing-their", "Inside JEB Feature: Parched cactus mice save water by curbing their appetite", target="_blank"),
       br(),
       tags$a(href="https://journals-biologists-com.unh.idm.oclc.org/jeb/article/226/23/jeb246936/335760/ECR-Spotlight-Danielle-Blumstein", "JEB ECR Spotlight – Danielle Blumstein", target="_blank"),
       br(),
-      tags$a(href="https://www.biorxiv.org/content/10.1101/2024.05.03.592397v1.abstract", "Impacts of dietary fat on multi tissue gene expression in the desert-adapted cactus mouse", target="_blank"),
+      tags$a(href="https://journals-biologists-com.unh.idm.oclc.org/jeb/article/226/23/jeb246386/335758/When-the-tap-runs-dry-the-physiological-effects-of", "When the tap runs dry: the physiological effects of acute experimental dehydration in Peromyscus eremicus ", target="_blank"),
       br(),
-      tags$a(href="https://www.biorxiv.org/content/10.1101/2024.01.22.576658v2.abstract", "When the tap runs dry: The multi-tissue gene expression and physiological responses of water deprived Peromyscus eremicus", target="_blank"),
+      tags$a(href="https://www.biorxiv.org/content/10.1101/2024.05.03.592397v1.abstract", "Impacts of dietary fat on multi tissue gene expression in the desert-adapted cactus mouse", target="_blank"),
       br(),
       tags$a(href="https://www.biorxiv.org/content/10.1101/2022.04.15.488461v3.abstract", "High total water loss driven by low-fat diet in desert-adapted mice", target="_blank"),
       br(),
-      tags$a(href="https://www.biorxiv.org/content/10.1101/2020.12.18.423523v1.abstract", "Disentangling environmental drivers of circadian metabolism in desert-adapted mice", target="_blank"),
+      tags$a(href="https://journals-biologists-com.unh.idm.oclc.org/jeb/article/224/18/jeb242529/272300/Disentangling-environmental-drivers-of-circadian", "Disentangling environmental drivers of circadian metabolism in desert-adapted mice", target="_blank"),
       br(),
-      tags$a(href="https://www.biorxiv.org/content/10.1101/2020.06.29.178392v3", "Limited evidence for parallel evolution among desert adapted Peromyscus deer mice", target="_blank"),
+      tags$a(href="https://academic-oup-com.unh.idm.oclc.org/jhered/article/112/3/286/6158454", "Limited evidence for parallel evolution among desert adapted Peromyscus deer mice", target="_blank"),
       br(),
       tags$a(href=" https://doi.org/10.1111/mec.15401", "Comparative and population genomics approaches reveal the basis of adaptation to deserts in a small rodent", target="_blank"),
       br(),
