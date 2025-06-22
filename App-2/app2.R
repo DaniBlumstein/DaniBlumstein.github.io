5b4d2b00b56a5ceae9cce69bc3e05d8eff79d30f
Dani Blumstein <49007024+DaniBlumstein@users.noreply.github.com>
2025-06-22 10:44:48 -0700

    updated files in graphing app

--- App-2/app2.R
+++ App-2/app2.R
@@ -37,11 +37,6 @@ ui <- fluidPage(
       }
     "))
   ),
-  fluidRow(
-    column(12, div(class = "main-title text-center",
-                   "Gene ontology results"))
-  ),
-  
   # Info Panel with Instructions and Links ----
   fluidRow(
     column(
@@ -70,9 +65,9 @@ ui <- fluidPage(
         )
       ),
       div(class = "sidebar-panel",
-          helpText("Explore the gene ontology results from either the diet experiment or the water deprivation expeiment."),
+          helpText("Explore the unfiltered gene ontology results from either the diet experiment or the water deprivation expeiment."),
           selectInput("dataset_choice", label = "Choose Dataset:",
-                      choices = c("diet" = "wgcna_data_go.csv",
+                      choices = c("diet" = "wgcna_data_go_diet.csv",
                                   "dehydration" = "go_wgcna_final_dehy.csv")),
           h5("Shiny app DOI"),
           tags$a(href="https://zenodo.org/badge/latestdoi/194934393", img(src="https://zenodo.org/badge/194934393.svg"), alt="DOI", target="_blank"),
@@ -94,6 +89,12 @@ ui <- fluidPage(
                Under baseline circadian environmental conditions, the chamber is held at a temperature of 32°C during the light phase and relative humidity of 10% from 09:00 to 20:00. 
                At 20:00, the chamber temperature is decreased and humidity gradually increased over the course of one hour to 24°C and 25% RH, with lights-out at 20:00. 
                After 21:00, conditions are held constant over the dark phase until 06:00. At 06:00, where the light phase is initiated and there is a gradual 3-hour transition from cool temperatures and higher humidity (24°C and 25% RH) to warmer temperatures and lower humidity (32°C and 10% RH)."),
+      br(),
+      p("In the water deprivation experiment, mice were provided a standard diet and fed ad libitum (LabDiet® 5015*, 26.101% fat, 19.752% protein, 54.148% carbohydrates, energy 15.02 kJ/g, food quotient [FQ] 0.89). Animals were randomly selected and assigned to the two water treatment groups, with or without water for three day. "),
+      br(),
+      p("In the diet experiment, animals provided with water ad libitum, were randomly assigned to one of two diet groups, a standard diet group [SD – LabDiet® 5015*: 26.101% fat, 19.752% protein, 54.148% carbohydrate, energy 15.02 kJ g−1, FQ=0.89] or a low-fat diet group [LF diet – Modified LabDiet® 5015 (5G0Z), 6.6% fat, 22.8% protein, 70.6% carbohydrate, energy 14.31 kJ g−1, FQ=0.92].Prior to the beginning of the experiment, mice were fed the assigned diet for 4 weeks"),
+      br(),
+      p("At the conclusion of the experiments, body temperature was recorded (only in the water deprivation study), mice were weighed, animals were euthanized with an overdose of isoflurane, and 120 µl of trunk blood was collected for serum electrolyte measurement and analyzed. We measured the concentration of sodium (Na, mmol/L), potassium (K, mmol/L), blood urea nitrogen (BUN, mmol/L), hematocrit (Hct, % PCV), ionized calcium (iCa, mmol/L), glucose (Glu, mmol/L), osmolality (mmol/L), hemoglobin (Hb, g/dl), chlorine (Cl, mEq/L), total CO2 (TCO2, mmol/L), and Anion gap (AnGap, mEq/L). Using Na, Glu, and BUN, we calculated serum osmolality. We calculated the mean of the last hour of water loss, EE, and RQ for each mouse."),
       br(),
       br(),
     )
